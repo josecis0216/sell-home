f70f97b731c8453491da8aa765f62084dbc0c675
Jose Cisneros <josecis0216@gmail.com>
2022-12-14 16:00:09 -0700

    adding new post to store isnt working

--- store/index.js
+++ store/index.js
@@ -7,26 +7,44 @@ const createStore = () => {
       loadedHomePosts: [],
     },
     mutations: {
-      setHomes(state, homes) {
-        state.loadedHomePosts = homes
+      setHomes(state, posts) {
+        state.loadedHomePosts = posts
       },
+      addHome(state, post) {
+        state.loadedHomePosts.push(post)
+      },
+      editHome(state, editedPost) {
+        const postIndex = state.loadedHomePosts.findIndex(post => post.id === editedPost)
+        state.loadedHomePosts[postIndex] = editedPost
+      }
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
         return axios.get('https://nuxt-blog-fdc27-default-rtdb.firebaseio.com/homes.json')
-        .then(res => {
+          .then(res => {
             const homesArray = []
             for (const key in res.data) {
-                homesArray.push({ ...res.data[key], id: key })
+              homesArray.push({ ...res.data[key], id: key })
             }
             vuexContext.commit('setHomes', homesArray)
-        })
-        .catch(e => context.error(e)
-        );
+          })
+          .catch(e => context.error(e)
+          );
       },
       setHomes(vuexContext, homes) {
         vuexContext.commit('setHomes', homes)
       },
+      addPost(vuexContext, post) {
+        const createdPost = { ...homeData, updatedDate: new Date() }
+        return axios.post('https://nuxt-blog-fdc27-default-rtdb.firebaseio.com/homes.json', createdPost)
+          .then(res => {
+            vuexContext.commit('addPost', { ...createdPost, id: res.data.name })
+          })
+          .catch(e => console.log(e))
+      },
+      editPost(vuexContext, editedPost) {
+
+      }
     },
     getters: {
       loadedHomePosts(state) {
